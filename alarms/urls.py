e7a8d24ca54506e7001805ca729d0a0fb3c026f8
Subin Siby <subins2000@gmail.com>
2019-01-20 10:09:33 +0530

    Add endpoint to view alarm info and delete it

--- alarms/urls.py
+++ alarms/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.AlarmsView.as_view()),
+    # a/ Alarm endpoint
+    path('a/<int:id>', views.AlarmView.as_view())
 ]
--- alarms/views.py
+++ alarms/views.py
@@ -7,3 +7,14 @@ from alarms.serializers import AlarmsSerializer
 class AlarmsView(generics.ListCreateAPIView):
     serializer_class = AlarmsSerializer
     queryset = Alarms.objects.all()
+
+
+class AlarmView(generics.RetrieveDestroyAPIView):
+    serializer_class = AlarmsSerializer
+
+    lookup_url_kwarg = 'id'
+
+    def get_queryset(self):
+        id = self.kwargs.get(self.lookup_url_kwarg)
+        alarm = Alarms.objects.filter(id=id)
+        return alarm
